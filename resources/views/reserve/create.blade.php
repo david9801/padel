e1bc9824efa99644d7264fb3dcaca8cc8531abc6
David Hermo Blanco <davidhermo.tei@gmail.com>
2023-02-11 17:34:34 +0100

    Added calendar

--- resources/views/reserve/create.blade.php
+++ resources/views/reserve/create.blade.php
@@ -7,10 +7,9 @@
             color: #333;
             border-radius: 5px;
             overflow: hidden;
-            margin-top: 110px;
+            margin-top: 100px;
             text-align: center;
         }
-
         #table-create thead th {
             background-color: #333;
             color: #fff;
@@ -34,9 +33,27 @@
         #table-create .bi {
             font-size: 9px;
         }
-        #court-selector{
-
+        #weeks {
+            margin-top: 90px;
+            text-align: center;
         }
+        #weeks .btn {
+            padding: 10px 20px;
+            font-size: 14px;
+            background-color: #333;
+            color: #fff;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 0 10px;
+        }
+
+        #weeks .btn:hover {
+            background-color: #fff;
+            color: #333;
+        }
+
+
     </style>
     @if ($errors->any())
         <div class="alert alert-danger">
@@ -47,68 +64,70 @@
             </ul>
         </div>
     @endif
+    <div class="text-center" id="weeks">
+        <button type="button" class="btn btn-primary" id="previous-week">Anterior Semana</button>
+        <button type="button" class="btn btn-primary" id="next-week">Siguiente Semana</button>
+    </div>
     <table class="table table-bordered" id="table-create">
         <thead>
-            <tr>
-                <th scope="col">Horario</th>
-                @for($i=1; $i<=7; $i++)
-                    <th scope="col">{{ trans("days.$i") }}</th>
-                @endfor
-            </tr>
+        <tr>
+            <th scope="col">Horario</th>
+            @for($i=1; $i<=7; $i++)
+                <th scope="col">{{ trans("days.$i") }}</th>
+            @endfor
+        </tr>
         </thead>
         <tbody>
-            @for($i = 11; $i < 21; $i++)
-                <tr>
-                    <td scope="row">{{ $i }}:00</td>
-                    @php
-                        $selectedDate = $i . ':00';
-                    @endphp
-                    <td>
-                        <a href="{{route('send', ['start_time' => $selectedDate])}}" method="get">
-                                @csrf
-                                <button class="btn btn-primary"><i class="bi bi-alarm-fill"></i></button>
-                            </a>
-                    </td>
-                    <td>
-                        <a href="{{route('send', ['start_time' => $selectedDate])}}" method="get">
-                            @csrf
-                            <button class="btn btn-primary"><i class="bi bi-alarm-fill"></i></button>
-                        </a>
-                    </td>
-                    <td>
-                        <a href="{{route('send', ['start_time' => $selectedDate])}}" method="get">
-                            @csrf
-                            <button class="btn btn-primary"><i class="bi bi-alarm-fill"></i></button>
-                        </a>
-                    </td>
-                    <td>
-                        <a href="{{route('send', ['start_time' => $selectedDate])}}" method="get">
-                            @csrf
-                            <button class="btn btn-primary"><i class="bi bi-alarm-fill"></i></button>
-                        </a>
-                    </td>
-                    <td>
-                        <a href="{{route('send', ['start_time' => $selectedDate])}}" method="get">
-                            @csrf
-                            <button class="btn btn-primary"><i class="bi bi-alarm-fill"></i></button>
-                        </a>
-                    </td>
-                    <td>
-                        <a
-                        <a href="{{route('send', ['start_time' => $selectedDate])}}" method="get">
-                            @csrf
-                            <button class="btn btn-primary"><i class="bi bi-alarm-fill"></i></button>
-                        </a>
-                    </td>
-                    <td>
-                        <a href="{{route('send', ['start_time' => $selectedDate])}}" method="get">
-                            @csrf
-                            <button class="btn btn-primary"><i class="bi bi-alarm-fill"></i></button>
-                        </a>
-                    </td>
-                </tr>
-            @endfor
+        @for($i = 11; $i < 21; $i++)
+            <tr>
+                <td scope="row">{{ $i }}:00</td>
+                @php
+                    $selectedDate = $i . ':00';
+                @endphp
+                <td>
+                    <a href="{{route('send', ['start_time' => $selectedDate])}}" method="get">
+                        @csrf
+                        <button class="btn btn-primary"><i class="bi bi-alarm-fill"></i></button>
+                    </a>
+                </td>
+                <td>
+                    <a href="{{route('send', ['start_time' => $selectedDate])}}" method="get">
+                        @csrf
+                        <button class="btn btn-primary"><i class="bi bi-alarm-fill"></i></button>
+                    </a>
+                </td>
+                <td>
+                    <a href="{{route('send', ['start_time' => $selectedDate])}}" method="get">
+                        @csrf
+                        <button class="btn btn-primary"><i class="bi bi-alarm-fill"></i></button>
+                    </a>
+                </td>
+                <td>
+                    <a href="{{route('send', ['start_time' => $selectedDate])}}" method="get">
+                        @csrf
+                        <button class="btn btn-primary"><i class="bi bi-alarm-fill"></i></button>
+                    </a>
+                </td>
+                <td>
+                    <a href="{{route('send', ['start_time' => $selectedDate])}}" method="get">
+                        @csrf
+                        <button class="btn btn-primary"><i class="bi bi-alarm-fill"></i></button>
+                    </a>
+                </td>
+                <td>
+                    <a href="{{route('send', ['start_time' => $selectedDate])}}" method="get">
+                        @csrf
+                        <button class="btn btn-primary"><i class="bi bi-alarm-fill"></i></button>
+                    </a>
+                </td>
+                <td>
+                    <a href="{{route('send', ['start_time' => $selectedDate])}}" method="get">
+                        @csrf
+                        <button class="btn btn-primary"><i class="bi bi-alarm-fill"></i></button>
+                    </a>
+                </td>
+            </tr>
+        @endfor
         </tbody>
+    </table>
 @endsection
-
-
